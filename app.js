6a26c5e3230576336cb6389bce49a5caaf8a89d8
sakhawat71 <sakhawat.showrav@gmail.com>
2023-04-01 00:32:18 +0600

    search result

--- app.js
+++ app.js
@@ -16,12 +16,27 @@ const searchSong = () =>{
 
 //2nd function
 const displaySong = songs =>{
-    console.log(songs);
-    const songContainer = document.getElementById("");
+
+    const songContainer = document.getElementById("song-container");
+    songContainer.innerHTML = ``;
+
     songs.forEach(songs => {
-        const li = document.createElement('li');
-        li.innerText = songs.titel;
-        
+        const SongsDiv = document.createElement('div');
+        SongsDiv.className = "single-result row align-items-center my-3 p-3";
+        SongsDiv.innerHTML = `
+            <div class="col-md-9">
+                <h3 class="lyrics-name">${songs.title}</h3>
+                <p class="author lead">Album by <span>${songs.artist.name}</span></p>
+                <audio controls>
+                    <source src="${songs.preview}" type="audio/mpeg">
+                </audio>
+                </div>
+            
+            <div class="col-md-3 text-md-right text-center">
+                <button class="btn btn-success">Get Lyrics</button>
+            </div>
+        `
+        songContainer.appendChild(SongsDiv);
     });
 
 }
